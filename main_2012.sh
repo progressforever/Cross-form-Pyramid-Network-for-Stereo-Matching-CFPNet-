96f5ea6253c97764ede2713f2736eaec2a82d846
Frank <37981708+progressforever@users.noreply.github.com>
2019-04-29 11:03:55 +0800

    Update main_2012.sh

--- main_2012.sh
+++ main_2012.sh
@@ -4,5 +4,4 @@ nohup python main.py --maxdisp 192 \
                --model stackhourglass\
                --datapath /home1/Documents/Database/Kitti2012/training/ \
                --epochs 600 \
-               --loadmodel ./train_model/flyingthings_final_4688514_epoch_40_model/checkpoint_22.tar \
-               --savemodel ./train_model/Data_2012_4688514_final_flying22_epoch_600_model/ > ./runLog2012/cfp/Data_2012_4688514_final_flying22_epoch_600_Run.log 2>&1 &
+               --savemodel ./models > ./Run.log 2>&1 &
